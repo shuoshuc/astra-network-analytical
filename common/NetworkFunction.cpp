ea524e342d3700ae4999c478baf2ceef9ada42a1
Max Tang <mhtang613@gmail.com>
2025-10-23 18:41:25 -0400

    fix include

--- common/NetworkFunction.cpp
+++ common/NetworkFunction.cpp
@@ -4,6 +4,7 @@ LICENSE file in the root directory of this source tree.
 *******************************************************************************/
 
 #include "common/NetworkFunction.h"
+#include "common/Flags.h"
 #include <cassert>
 #include <iostream>
 
